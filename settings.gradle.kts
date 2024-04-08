c9c0a33d30145c6efc19d1d0d67438bbcb4c2057
stayforyou <bread.s@kakao.com>
2024-04-09 05:55:19 +0900

    프론트 제거

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "supreme-time-deal"
+
+include(
+	"app-authentication",
+	"app-order",
+	"app-customer",
+	"app-product",
+	"app-api-gateway",
+	"support",
+	"domain-logic-experiment",
+)
