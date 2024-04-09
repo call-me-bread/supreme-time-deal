efef455f6e699457fb24cc7f87548922decfb71a
stayforyou <bread.s@kakao.com>
2024-04-09 10:19:25 +0900

    테스트 오류 수정

--- domain-logic-experiment/src/test/kotlin/me/bread/supreme/integration/application/AccountsServiceTest.kt
+++ domain-logic-experiment/src/test/kotlin/me/bread/supreme/integration/application/AccountsServiceTest.kt
@@ -3,14 +3,15 @@ package me.bread.supreme.integration.application
 import io.mockk.mockk
 import me.bread.supreme.integration.application.business.AuthService
 import me.bread.supreme.integration.domain.accounts.exception.NoAuthorizationException
+import me.bread.supreme.integration.domain.accounts.repsository.AccountsRepository
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
-class AuthServiceTest {
+class AccountsServiceTest {
 
-	private val authRepository: AuthRepository = mockk()
-	private val authService = AuthService(authRepository)
+	private val accountsRepository: AccountsRepository = mockk()
+	private val authService = AuthService(accountsRepository)
 
 	@Test
 	fun `fail when create customer with invalid token`() {
